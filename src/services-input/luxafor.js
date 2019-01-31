025a6b80ac331cddae9cba7219e5e20ff8094eb7
Peter Goes <petergoes@gmail.com>
2019-01-31 10:16:24 +0100

    check if luxafor devices is connected before use

--- src/services-input/luxafor.js
+++ src/services-input/luxafor.js
@@ -2,29 +2,48 @@ import { app } from 'electron'
 import Luxafor from 'node-luxafor'
 import status from '../main/status'
 
-export default function luxafor() {
-    try {
-        const light = new Luxafor()
-        const off = [0, 0, 0, 25]
-        const red = [255, 0, 0, 25]
-        const green = [0, 255, 0, 25]
-        const blue = [0, 0, 255, 25]
-        light.fade(...green).exec()
+let luxaLight;
 
-        status.on('statusStarts', () => {
-            status.dnd 
-                ? light.fade(...red).exec()
-                : light.fade(...blue).exec()
-        })
-
-        status.on('statusEnds', () => {
-            light.fade(...green).exec()
-        })
-
-        app.on('before-quit', () => {
-            light.fade(...off).exec()
-        })
-    } catch (err) {
-        console.log(err.message)
+async function getLight() {
+    if (!luxaLight) {
+        luxaLight = new Luxafor()
     }
+    return luxaLight
+}
+
+function clearLight() {
+    luxaLight = null;
+}
+
+export default async function luxafor() {
+    const off = [0, 0, 0, 25]
+    const red = [255, 0, 0, 25]
+    const green = [0, 255, 0, 25]
+    const blue = [0, 0, 255, 25]
+
+    getLight()
+        .then(light => light.fade(...green).exec())
+        .catch(clearLight)
+
+    status.on('statusStarts', () => {
+        getLight()
+            .then(light => {
+                status.dnd 
+                    ? light.fade(...red).exec()
+                    : light.fade(...blue).exec()
+            })
+            .catch(clearLight)
+    })
+
+    status.on('statusEnds', () => {
+        getLight()
+            .then(light => light.fade(...green).exec())
+            .catch(clearLight)
+    })
+
+    app.on('before-quit', () => {
+        getLight()
+            .then(light => light.fade(...off).exec())
+            .catch(clearLight)
+    })
 }
