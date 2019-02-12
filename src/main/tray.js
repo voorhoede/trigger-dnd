7312a7976e59108c641a48d62f791ae1c5ef9977
Peter Goes <petergoes@gmail.com>
2019-02-12 13:00:09 +0100

    add blue tray icon
    closes #9

--- src/main/tray.js
+++ src/main/tray.js
@@ -4,6 +4,7 @@ import status from './status'
 
 const iconOffWhite = path.join(__dirname, '../assets/off.png')
 const iconOnWhite = path.join(__dirname, '../assets/on-white.png')
+const iconOnBlue = path.join(__dirname, '../assets/on-blue.png')
 const iconOffDark = path.join(__dirname, '../assets/off.png')
 const iconOnDark = path.join(__dirname, '../assets/on-black.png')
 
@@ -14,7 +15,8 @@ systemPreferences.subscribeNotification(
 	setIconToTray
 )
 
-status.on('dnd', setIconToTray)
+status.on('statusStarts', setIconToTray)
+status.on('statusEnds', setIconToTray)
 
 export default function createTray(contextMenu) {
 	const icon = getCorrectIcon()
@@ -28,6 +30,8 @@ function getCorrectIcon() {
 		return systemPreferences.isDarkMode() 
 			? iconOffWhite
 			: iconOffDark
+	} else if (status.endTime) {
+		return iconOnBlue
 	} else {
 		return systemPreferences.isDarkMode() 
 			? iconOnWhite
